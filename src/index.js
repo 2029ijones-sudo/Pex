403d4ba7e2e8d1a77569475d186e48b74004f866
2029ijones-sudo <2029ijones@wfschools.org>
2025-12-27 12:39:21 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+// src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create root container (React 18)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the App
+root.render(<App />);
